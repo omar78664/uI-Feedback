96603c7b068945722989dcfa87530f9a64de557b
omar78664 <108195647+omar78664@users.noreply.github.com>
2022-11-21 13:11:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { v4 as uuidv4 } from 'uuid'
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackSate from "./components/FeedbackStat.jsx"
+import FeedbackForum from "./components/FeedbackForum"
+import FeedbackData from "./data/FeedbackData"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [feedback, setFeedback] = useState(FeedbackData) 
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure?')){
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  return(
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForum handleAdd={addFeedback} />
+        <FeedbackSate feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
